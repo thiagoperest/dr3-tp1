793fb884b54fd46a927cf7b1800ea7d289c5e709
thiagoperest <thiago_teodoroperes07@hotmail.com>
2025-08-06 17:56:22 -0300

    test: add base inicial de teste

---
+++ src/test/java/ScientificCalculatorTest.java
@@ -0,0 +1,17 @@
+import br.edu.infnet.ScientificCalculator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("Testes da Calculadora Científica - DR3 - TP1")
+public class ScientificCalculatorTest {
+
+    private ScientificCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new ScientificCalculator();
+    }
+
+    // TODO: Implementar testes
+
+}
